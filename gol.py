f700b131baf23649c0e679cc5935bcd0eb47f315
df@ <@>
2021-09-28 20:13:06 +0100

    ok

--- gol.py
+++ gol.py
@@ -64,8 +64,8 @@ def print_world(world, gen):
 def save_image(world, gen):
     img = Image.new('RGB', (HEIGHT, WIDTH))
     imgname = 'life.{:>04d}.png'.format(gen)
-    for x in range(len(world)-1):
-        for y in range(len(world[x])-1):
+    for x in range(len(world)):
+        for y in range(len(world[x])):
             r = 0 if world[x][y] == DEAD else 255
             img.putpixel((x, y), (r, r, r))
     img.save(imgname)
