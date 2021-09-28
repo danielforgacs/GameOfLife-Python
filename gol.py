30d6450a78459d2b745b02b903d1bff007f53b32
df@ <@>
2021-09-28 14:47:32 +0100

    ok

--- gol.py
+++ gol.py
@@ -1,75 +1,55 @@
 import random
 import time
 
-WIDTH = 4
-
 def gen_map():
-    map_x = []
-    for k in range(WIDTH):
-        map_y = []
-        for j in range(WIDTH):
-            map_y.append(random.choice([0, 1]))
-        map_x.append(map_y)
-    return map_x
-
-def calc_neighbours(world, x, y):
-    count = 0
-    for k in [-1, 0, 1]:
-        for i in [-1, 0, 1]:
-            x2 = x + k
-            y2 = y+ i
-            if x == 0 or x == len(world[k])-1:
-                continue
-            if y == 0 or y == len(world[k])-1:
-                continue
-            if (x2, y2) == (x, y):
-                continue
-            if world[x2][y2] == 1:
-                count += 1
-    # print(count)
-    return count
+    side = 4
+    map_x = ()
+    for k in range(side):
+        map_y = ()
+        for j in range(side):
+            map_y = map_y + (random.choice([0, 1]), )
+        map_x = map_x + (map_y,)
+    return map_x, 0
 
 
-def calc_time_step(world):
-    newworld = [[0]*WIDTH]*WIDTH
-    for k in range(WIDTH):
-        for j in range(WIDTH):
-            neighbour_count = calc_neighbours(world, k, j)
-            # print (neighbour_count)
-            # if world[k][j] == 1:
-            #     if neighbour_count in [0, 1]:
-            #         newworld[k][j] = 0
-            #     if neighbour_count in [2, 3]:
-            #         newworld[k][j] = 1
-            # else:
-            #     if neighbour_count == 3:
-            #         newworld[k][j] = 1
-            newworld[k][j] = neighbour_count
-    return newworld
+def calc_time_step(world, gen):
+    newworld = ()
+    for x in range(len(world)):
+        newline = ()
+        for y in range(len(world[x])):
+            neibours = 0
+            if world[x][y] == 1:
+                if neibours in [0, 1]:
+                    newvalue = 0
+                if neibours in [2, 3]:
+                    newvalue = 1
+            else:
+                if neibours == 3:
+                    newvalue = 1
+                else:
+                    newvalue = 0
+            newline = newline+(newvalue,)
+        newworld = newworld+ (newline,)
+    return newworld, gen+1
 
-def print_world(world):
-    for k in range(WIDTH):
-        print(world[k])
-    time.sleep(0.1)
-    print()
+def print_world(world, gen):
+    print('gen:', gen)
+    for k in world:
+        print(k)
 
 def main():
-    gen = 0
-    # world = gen_map()
+    world, gen = gen_map()
     world = (
         (0, 0, 0, 0),
-        (0, 0, 0, 0),
+        (0, 1, 0, 0),
         (0, 0, 0, 0),
         (0, 0, 0, 0),
     )
-    print('gen:', gen)
-    print_world(world)
+    print_world(world, gen)
 
     for k in range(1):
-        world = calc_time_step(world)
-        gen += 1
-        print('gen:', gen)
-        print_world(world)
+        world, gen = calc_time_step(world, gen)
+        print_world(world, gen)
 
 
 main()
