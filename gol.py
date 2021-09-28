e85ad91a5ac5a2b9b821cf67c42b1ed7b77debf0
df@ <@>
2021-09-28 14:29:33 +0100

    //

--- gol.py
+++ gol.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-WIDTH = 10
+WIDTH = 4
 
 def gen_map():
     map_x = []
@@ -9,61 +9,66 @@ def gen_map():
         map_y = []
         for j in range(WIDTH):
             map_y.append(random.choice([0, 1]))
-
         map_x.append(map_y)
-
     return map_x
+
+def calc_neighbours(world, x, y):
+    count = 0
+    for k in [-1, 0, 1]:
+        for i in [-1, 0, 1]:
+            x2 = x + k
+            y2 = y+ i
+            if x == 0 or x == len(world[k])-1:
+                continue
+            if y == 0 or y == len(world[k])-1:
+                continue
+            if (x2, y2) == (x, y):
+                continue
+            if world[x2][y2] == 1:
+                count += 1
+    # print(count)
+    return count
+
 
 def calc_time_step(world):
     newworld = [[0]*WIDTH]*WIDTH
     for k in range(WIDTH):
         for j in range(WIDTH):
-            neighbour_count = 0
-            for x_n in [-1, 0, 1]:
-                for y_n in [-1, 0, 1]:
-                    n_current_x = k+x_n
-                    n_current_y = j+y_n
-                    if n_current_x < 0 or n_current_x > WIDTH-1:
-                        continue
-                    if n_current_y < 0 or n_current_y > WIDTH-1:
-                        continue
-                    if world[n_current_x][n_current_y] == 1:
-                        neighbour_count += 1
-
-            if world[k][j] == 1:
-                if neighbour_count in [0, 1]:
-                    newworld[k][j] = 0
-                if neighbour_count in [2, 3]:
-                    newworld[k][j] = 1
-            else:
-                if neighbour_count == 3:
-                    newworld[k][j] = 1
+            neighbour_count = calc_neighbours(world, k, j)
+            # print (neighbour_count)
+            # if world[k][j] == 1:
+            #     if neighbour_count in [0, 1]:
+            #         newworld[k][j] = 0
+            #     if neighbour_count in [2, 3]:
+            #         newworld[k][j] = 1
+            # else:
+            #     if neighbour_count == 3:
+            #         newworld[k][j] = 1
+            newworld[k][j] = neighbour_count
     return newworld
 
 def print_world(world):
-    print()
     for k in range(WIDTH):
         print(world[k])
-    time.sleep(1)
+    time.sleep(0.1)
+    print()
 
 def main():
-    world = gen_map()
-    world = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ]
+    gen = 0
+    # world = gen_map()
+    world = (
+        (0, 0, 0, 0),
+        (0, 0, 0, 0),
+        (0, 0, 0, 0),
+        (0, 0, 0, 0),
+    )
+    print('gen:', gen)
     print_world(world)
 
     for k in range(1):
         world = calc_time_step(world)
+        gen += 1
+        print('gen:', gen)
         print_world(world)
 
 
